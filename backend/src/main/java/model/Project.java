50ecf603b66e84649a30faf58a685c5725e23ae1
Kheder Murad <kheder99@gmx.de>
2024-10-27 22:59:54 +0100

    Added model classes for ORM

---
+++ backend/src/main/java/model/Project.java
@@ -0,0 +1,97 @@
+package model;
+
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "start_date")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date startDate;
+	
+	@Column(name = "end_date")
+	@Temporal(TemporalType.DATE)
+	private Date endDate;
+	
+	@OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Task> tasks;
+	
+	public Project() {
+		
+	}
+	
+	
+	public Project(String name, Date startDate, Date endDate, List<Task> tasks) {
+		super();
+		this.name = name;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.tasks = tasks;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public List<Task> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+
+	
+	
+	
+
+	
+}
---
+++ backend/src/main/java/model/Task.java
@@ -0,0 +1,80 @@
+package model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name="project_id", nullable = false)
+	private Project project;
+	
+	@Column(name = "status")
+	@Enumerated(EnumType.STRING)
+	private TaskStatus status;
+	
+	
+	public Task() {
+		
+	}
+
+	public Task(String description, Project project, TaskStatus status) {
+		super();
+		this.description = description;
+		this.project = project;
+		this.status = status;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
+	public TaskStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(TaskStatus status) {
+		this.status = status;
+	}
+	
+	
+}
+
+
