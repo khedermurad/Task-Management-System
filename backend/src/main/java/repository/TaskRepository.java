b8f178c89fca13f647a5e505b276ac0fed9a1f10
Kheder Murad <kheder99@gmx.de>
2024-10-27 23:20:54 +0100

    added repositories to perform CRUD operations

---
+++ backend/src/main/java/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
